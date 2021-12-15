17af49168a5f76048249ed698afd446fa84d3400
damngoodpizza <96083179+damngoodpizza@users.noreply.github.com>
2021-12-15 11:06:33 -0600

    Update email_slicer.py

--- email_slicer.py
+++ email_slicer.py
@@ -2,19 +2,20 @@
 
 # declare a variable to store user input
 # remove inadvertent leading and trailing whitespace using the strip() function
-# The user may accidentally input spaces before or after their email address.
-# We can utilize the strip string method 'strip()' to remove unwanted spaces.
-
 email = input("What is your email address?: ").strip()
 
-# Slice out user name
+# Slice out user name and store in variable
 user = email[: email.index("@")]
 
-# Slice out domain name
+# Slice out domain name and store in variable
 domain = email[email.index("@") + 1 :]
 
 # Format message
-output = "Your username is {} and your domain name is {}".format(user, domain)
+# output = "Your username is {} and your domain name is {}".format(user, domain)
+
+# NOTE: Since the introduction of f-strings in Python 3.6, the correct way to
+# do this would be:
+output = f"Your username is [{user}] and your domain name is [{domain}]"
 
 # Display output message
 print(output)
