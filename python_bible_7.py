68456b2cc998e3dadf0934a5c0c1dd07d5b874e2
damngoodpizza <sniderman@gmail.com>
2021-12-19 19:17:12 -0600

    update

--- python_bible_7.py
+++ python_bible_7.py
@@ -57,15 +57,15 @@ cities = ["Atlanta", "New York", "Philadelphia", "Los Angeles", "Detroit", "Tamp
 # NOTE: You must be able to address the specific element. If the exact element is
 # unknown, this won't work.
 print(cities)
-output = cities.remove("Atlanta")
-print(output)
+cities.remove("Atlanta")
+print(cities)
 
 
 # Sometimes, you need to remove a certain index, or slice of indexes, from a list.
 # We can delete index zero like so:
-list = [1, 2, 3, 4, 5]
-del list[0]
-print(list)
+example = [1, 2, 3, 4, 5]
+del example[0]
+print(example)
 
 # Unlike the remove method, the del method can target a specific index:
 example = ["a", "b", "c", "a", "b"]
@@ -81,4 +81,119 @@ print("And use del to target the second instance of 'a': " + str(example))
 example = ["a", "b", "c", "a", "b"]
 del example[1:4]
 print("In this example, we slice 'b, c, and a' from the middle of the list" + str(example))
+
+# More ways to add items to lists:
+# Only lists can be added to lists using the addition operator.
+a = [5, 12, 72, 55, 89]
+a = a + [1]
+print(a)
+
+# What if we want to add a string to our list?
+a = a + ["test"]
+print(a)
+
+# We can also use the list method, but the behavior is different:
+a = a + list("test")
+print(a)
+# NOTE: This method doesn't work for integers, since they aren't an iterable data type, and
+# therefore cannot be converted into lists.
+
+# We could still add numbers as a string like so:
+a = a + list(str(123))
+print(a)
+# NOTE: Pay attention to the quotes around these numbers. These are strings, not integers.
+
+# What if I wanted to add some integers as a single element?
+# reset 'a'
+a = [5, 12, 72, 55, 89, 1]
+a = a + [[5,6,7,8]]
+print(a)
+
+# You could also use the append method:
+a.append([10,11,12,13])
+print(a)
+
+# What if we want to insert 100 into our list between 12 and 72?
+# reset 'a'
+a = [5, 12, 72, 55, 89, 1]
+a.insert(2,100)
+print(a)
+
+# How about inserting a slice?
+a.insert(2,[10,20,30])
+print(a)
+
+# !!!!! SUPER IMPORTANT !!!!!
+# Lists are mutable, and can therefore be overwritten. For example, why does the following work?
+a = "text string"
+a = a + " new"
+print(a)
+# And the following doesn't?
+a = [1,2,3]
+a = a.append(4)
+print(a)
+a = type(a)
+print(a)
+# This is because a.append(4) returns a value of 'None', overwriting the value of 'a' with 'None'
+
+
+
+# What Are Tuples
+# -------------------------------------------------------------------
+
+# Tuples are an immutable data type
+# Tuples are an iterable data type
+
+our_tuple = 1,2,3,"a","b","c"
+result = type(our_tuple)
+print(result)
+print(our_tuple)
+
+# While the above works, it is more common to surround our tuple with parenthesis for readability.
+our_tuple = (1,2,3,"a","b","c")
+print(result)
+print(our_tuple)
+
+# Being an iterable data type, we can look at specific elements or slices:
+result = our_tuple[0:3]
+print(result)
+
+# The only real difference between tuples and lists is that you cannot change a tuple after creation.
+# Tuples, like strings, are immutable.
+our_list = [1,2,3,4,5,6,7]
+our_list[2] = 100
+print(our_list)
+# We can change the 2nd element in our_list to 100.
+# our_tuple = (1,2,3,4,5,6,7)
+# our_tuple[2] = 100
+# print(our_tuple)
+# When we try to do the same with our_tuple, we get an error as the tuple object doesn't support
+# item assignment because it is immutable.
+
+# You can use the tuple() function to convert other data types to a tuple.
+a = [1,2,3]
+print(a)
+a = tuple(a)
+print(a)
+result = type(a)
+print(result)
+
+# You can do multiple assignment with tuples:
+(a,b,c) = 1,2,3
+print(a)
+print(b)
+print(c)
+
+# You can also do multiple assignment with lists:
+d,e,f = [4,5,6]
+print(d)
+print(e)
+print(f)
+
+# You can also do multiple assignment with strings:
+g,h,i = "789"
+print(g)
+print(h)
+print(i)
+
 
